ba748cf52a8128ea8c028e31e09acf6d24626530
yatin-kapur <yatin.aol@gmail.com>
2017-06-27 23:33:43 -0400

    better way to filter years

--- week1/le-vs-hc-spending.py
+++ week1/le-vs-hc-spending.py
@@ -36,19 +36,16 @@ health_spending = pd.read_csv("hc-exp.csv")
 del health_spending['Country code']
 health_spending = health_spending.rename(columns = {'Entity': 'Country Name'})
 
-# defining years to merge on
-years = np.arange(2002, 2014, 1)
-
 # removing elements that are not in the years range
 for i in range(len(health_spending.loc[:, 'Year'])):
-    if health_spending.loc[i, 'Year'] not in years:
+    if health_spending.loc[i, 'Year'] < 2002 and health_spending.loc[i, 'Year'] > 2014:
         health_spending.loc[i, 'Year'] = np.nan
 
 health_spending = health_spending.dropna()
 health_spending['Year'] = health_spending['Year'].apply(np.int64)
 
 for i in range(len(life_expectancy.loc[:, 'Year'])):
-    if life_expectancy.loc[i, 'Year'] not in years:
+    if life_expectancy.loc[i, 'Year'] < 2002 and life_expectancy.loc[i, 'Year'] > 2014:
         life_expectancy.loc[i, 'Year'] = np.nan
 
 life_expectancy = life_expectancy.dropna()
